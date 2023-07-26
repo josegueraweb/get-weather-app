e083846fbd855dba8dcb041ceafcf44ede3572fb
Oseguera, Joshua <joshoseguera09@gmail.com>
2023-07-26 08:11:52 -0700

    Added JavaScript code that parses and displays weather API data to the 'Today's Forecast' section of the web app. Specifically, the temperature, wind speed, and humidity data.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,43 +1,56 @@
-// var cityNameId = document.getElementById('city-name');
-// var cityTempId = document.getElementById('city-temp');
+  var form = document.getElementById('city-search-form');
+  var dayMainConditionEl = $('.day-main-condition');
+  var apiKey = "dc1a4138ca5b423d48372a11c0f7cf5c";
 
-var form = document.getElementById('city-search-form');
-var apiKey = "dc1a4138ca5b423d48372a11c0f7cf5c";
+  function getApi(event) {
+    event.preventDefault();
 
-function getApi(event) {
-  event.preventDefault();
+    var citySearchInput = document.getElementById('city-search-input').value.toLowerCase().trim();
 
-  var citySearchInput = document.getElementById('city-search-input').value.toLowerCase().trim();
+    var cityNameToGeocode = "http://api.openweathermap.org/geo/1.0/direct?q=" + citySearchInput + "&appid=" + apiKey;
 
-  var cityNameToGeocode = "http://api.openweathermap.org/geo/1.0/direct?q=" + citySearchInput + "&appid=" + apiKey;
+    fetch(cityNameToGeocode)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
 
-  fetch(cityNameToGeocode)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
+        var latitude = data[0].lat;
+        var longitude = data[0].lon;
 
-      for (var i = 0; i < data.length; i++) {
-        var latitude = data[i].lat;
-        var longitude = data[i].lon;
-      };
+        var cityWeatherCurrent = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial";
 
-      var cityWeatherCurrent = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial";
+        fetch(cityWeatherCurrent)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (weatherData) {
+            console.log(weatherData);
 
-      console.log(cityWeatherCurrent);
+            var currentTemp = weatherData.current.temp;
+            var currentWindSpeed = weatherData.current.wind_speed;
+            var currentHumidity = weatherData.current.humidity;
 
-    });
-  }
+            $('.current-temp').text("Temperature: " + currentTemp + " °F");
+            $('.current-wind-speed').text("Wind Speed: " + currentWindSpeed + " mph");
+            $('.current-humidity').text("Humidity: " + currentHumidity + "   (lbs/ft3)");
 
-//   fetch(cityWeatherCurrent)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     });
+          });
 
-// };
 
-form.addEventListener('submit', getApi);
+
+
+
+
+      });
+
+
+
+
+
+
+
+  };
+
+  form.addEventListener('submit', getApi);
